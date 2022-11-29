65919d5df15f232985054dff2113929091a917d4
JoseGomes-SG <joseguilhermecgomes4@gmail.com>
2022-11-28 23:29:28 -0300

    Atualiza

--- src/Radio.js
+++ src/Radio.js
@@ -3,51 +3,55 @@ import { RadioBrowserApi } from "radio-browser-api";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import defaultImage from "./radio.jpg";
+import stations from "./brStations.json";
 
 export default function Radio() {
-  const [stations, setStations] = useState();
+  /* const [stations, setStations] = useState(); 
 
   useEffect(() => {
     setupApi().then((data) => {
       setStations(data);
     });
   }, []);
+*/
 
+  /* 
   const setupApi = async () => {
     const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
 
     const stations = await api.searchStations({
       country: "Brazil",
-      limit: 6,
+      limit: 90,
     });
     return stations;
   };
-
+*/
   const setDefaultSrc = (event) => {
     event.target.src = defaultImage;
   };
 
+  let estacao = [];
+
   return (
     <div className="radio">
       <div className="stations">
-        {stations &&
-          stations.map((station, index) => {
-            console.log([station.name, station.urlResolved]);
-            return (
-              <div className="station" key={index}>
+        {Object.keys(stations).map((key) => {
+          for (var i in stations[key]) {
+            estacao.push(
+              <div className="station" key={stations[key][i].id}>
                 <div className="stationName">
                   <img
                     className="logo"
-                    src={station.favicon}
+                    src={stations[key][i].favicon}
                     alt="station logo"
                     onError={setDefaultSrc}
                   />
-                  <div className="name">{station.name}</div>
+                  <div className="name">{stations[key][i].name}</div>
                 </div>
 
                 <AudioPlayer
                   className="player"
-                  src={station.urlResolved}
+                  src={stations[key][i].url}
                   showJumpControls={false}
                   layout="stacked"
                   customProgressBarSection={[]}
@@ -56,7 +60,9 @@ export default function Radio() {
                 />
               </div>
             );
-          })}
+          }
+          return <>{estacao}</>;
+        })}
       </div>
     </div>
   );
