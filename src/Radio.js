f59227b4dd27d4c0c8fd1fe97b1b980b4dee9ce8
JoseGomes-SG <joseguilhermecgomes4@gmail.com>
2022-12-19 00:31:02 -0300

    atualizacao

--- src/Radio.js
+++ src/Radio.js
@@ -1,67 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { RadioBrowserApi } from "radio-browser-api";
+import React, { useState } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import defaultImage from "./radio.jpg";
-import stations from "./brStations.json";
+import data from "./data";
 
 export default function Radio() {
-  /* const [stations, setStations] = useState(); 
+  // Imprimir as estações
 
-  useEffect(() => {
-    setupApi().then((data) => {
-      setStations(data);
-    });
-  }, []);
-*/
+  const [estacoes] = useState(data());
 
-  /* 
-  const setupApi = async () => {
-    const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
-
-    const stations = await api.searchStations({
-      country: "Brazil",
-      limit: 90,
-    });
-    return stations;
-  };
-*/
   const setDefaultSrc = (event) => {
     event.target.src = defaultImage;
   };
 
-  let estacao = [];
-
   return (
     <div className="radio">
       <div className="stations">
-        {Object.keys(stations).map((key) => {
-          for (var i in stations[key]) {
-            estacao.push(
-              <div className="station" key={stations[key][i].id}>
-                <div className="stationName">
-                  <img
-                    className="logo"
-                    src={stations[key][i].favicon}
-                    alt="station logo"
-                    onError={setDefaultSrc}
-                  />
-                  <div className="name">{stations[key][i].name}</div>
-                </div>
-
-                <AudioPlayer
-                  className="player"
-                  src={stations[key][i].url}
-                  showJumpControls={false}
-                  layout="stacked"
-                  customProgressBarSection={[]}
-                  customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
-                  autoPlayAfterSrcChange={false}
+        {estacoes.map((index) => {
+          return (
+            <div className="station" key={index.id}>
+              <div className="stationName">
+                <img
+                  className="logo"
+                  src={index.favicon}
+                  alt="station logo"
+                  onError={setDefaultSrc}
                 />
+                <div className="name">{index.name}</div>
               </div>
-            );
-          }
-          return <>{estacao}</>;
+
+              <AudioPlayer
+                className="player"
+                src={index.url}
+                showJumpControls={false}
+                layout="stacked"
+                customProgressBarSection={[]}
+                customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                autoPlayAfterSrcChange={false}
+              />
+            </div>
+          );
         })}
       </div>
     </div>
---
+++ src/data.js
@@ -0,0 +1,86 @@
+function estacoes() {
+  return [
+    {
+      id: "0",
+      name: "Bossa Jazz Brasil",
+      url: "https://centova5.transmissaodigital.com:20104/live",
+      favicon:
+        "https://bossajazzbrasil.com/wp-content/uploads/2020/12/bjs-app.png",
+    },
+    {
+      id: "1",
+      name: "_101.3 BRAZİL FM",
+      url: "https://canli.arabeskinmerkezi.com/9180/stream",
+      favicon: "https://i.hizliresim.com/bvwiekr.png",
+    },
+    {
+      id: "2",
+      name: "Aliança Fm",
+      url: "https://server01.srvsh.com.br:13788/stream",
+      favicon: "",
+    },
+    {
+      id: "3",
+      name: "Brasil de Fato",
+      url: "https://s09.hstbr.net:8238/live?1514038524061",
+      favicon: "https://www.brasildefato.com.br//img/icons/mstile-144x144.png",
+    },
+    {
+      id: "4",
+      name: "Assembléia de Deus",
+      url: "https://ec2.yesstreaming.net:3925/stream",
+      favicon:
+        "https://assembleia.org.br/wp-content/uploads/2020/01/cropped-icon512-192x192.png",
+    },
+    {
+      id: "5",
+      name: "Bons Tempos FM",
+      url: "https://server02.ouvir.radio.br:8050/stream?1609720768800",
+      favicon: "https://img.radios.com.br/radio/lg/radio37027_1516218896.jpg",
+    },
+    {
+      id: "6",
+      name: "AKIBA",
+      url: "https://ssl.srvstm.com:9526/stream/1/",
+      favicon: "https://us0-cdn.onlineradiobox.com/img/l/0/67700.v9.png",
+    },
+    {
+      id: "7",
+      name: "Super Rádio Tupi 96.5 FM",
+      url: "https://8923.brasilstream.com.br/stream",
+      favicon: "https://img.radios.com.br/radio/lg/radio14215_1562877228.jpg",
+    },
+    {
+      id: "8",
+      name: "Jovem Pan",
+      url: "https://playerservices.streamtheworld.com/api/livestream-redirect/JP_SP_AMAAC.aac",
+      favicon: "https://img.radios.com.br/radio/lg/radio8800_1542619415.jpg",
+    },
+    {
+      id: "9",
+      name: "Rádio 99 FM",
+      url: "http://hts02.kshost.com.br:8862/live",
+      favicon: "https://img.radios.com.br/radio/lg/radio8901_1439401342.JPG",
+    },
+    {
+      id: "10",
+      name: "Rádio Aparecida 104.3 FM",
+      url: "https://aparecida.jmvstream.com/stream",
+      favicon: "https://img.radios.com.br/radio/lg/radio9571_1614260489.png",
+    },
+    {
+      id: "11",
+      name: "Rádio Globo RJ 98.1 FM",
+      url: "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_GLOBO_RJAAC.aac?dist=radioscombr",
+      favicon: "https://img.radios.com.br/radio/lg/radio8817_1645528697.png",
+    },
+    {
+      id: "12",
+      name: "Rádio Clube do Pará 104.7 FM",
+      url: "https://ice01.kshost.com.br:8000/live",
+      favicon: "https://img.radios.com.br/radio/lg/radio11886_1665487395.jpg",
+    },
+  ];
+}
+
+export default estacoes;
